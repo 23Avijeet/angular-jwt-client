c9afa5b0d8c481b879ad69a30afb3f3b68a00452
raza <pathantcs@gmail.com>
2020-11-19 08:56:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { SecurityComponent } from './security/security.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SecurityComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/jwt-client.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JwtClientService {
+  constructor(private httpClient: HttpClient) {}
+
+  public generateToken(request) {
+    return this.httpClient.post<string>(
+      'http://localhost:8081/authenticate',
+      request,
+      { responseType: 'text' as 'json' }
+    );
+  }
+
+  public welcome(token) {
+    const tokenStr = 'Bearer ' + token;
+    const headers = new HttpHeaders().set('Authorization', tokenStr);
+    return this.httpClient.get<string>('http://localhost:8081/hello', {
+      headers,
+      responseType: 'text' as 'json',
+    });
+  }
+}
